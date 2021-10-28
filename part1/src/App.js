3b4c0e04c83dc2979073b12d508761ad23a7d48e
Giorgi <sheklashviligiorgi@gmail.com>
2021-10-28 14:47:04 +0300

    Parts added

--- part1/src/App.js
+++ part1/src/App.js
@@ -14,19 +14,19 @@ const App = () => {
   )
   const Content = (props) => (
     <>
-      <p>
-        {props.part1} {props.exercises1}
-      </p>
-      <p>
-        {props.part2} {props.exercises2}
-      </p>
-      <p>
-        {props.part3} {props.exercises3}
-      </p>
+       <Part part={part1} exercise={exercises1} />
+       <Part part={part2} exercise={exercises2} />
+       <Part part={part3} exercise={exercises3} />
     </>
   )
   const Total = (props) => (
     <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
+  )
+
+  const Part = (props) => (
+    <p>
+      {props.part} {props.exercise}
+    </p>
   )
 
   return (
